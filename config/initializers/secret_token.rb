a03858016672a1f2a8435300f106dcbe292424dc
Seth Dowden <sethclarkdowden@gmail.com>
2012-08-04 12:34:31 -0700

    fancy first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jfbk::Application.config.secret_token = '6addd2ae5002c35ccd2b0e0d88854008e6f7a2dbc03a924528e75e067694e6a07b49230661807e4cfa223b78998a7d1aec052bf475e0a96eb4df6a27487c16f8'
